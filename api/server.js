481bb01dcb4b50e518bf7d58d3636f7e10c7c767
garabedium <ggarabedian09@gmail.com>
2019-10-06 18:27:40 -0400

    connect to db

--- api/server.js
+++ api/server.js
@@ -1,11 +1,20 @@
 //server.js
 const express = require('express'),
-      { port } = require('./config'),
+      mysql = require('mysql'),
+      { port, host, db, db_user, db_pwd } = require('./config'),
       app = express(),
       bodyParser = require('body-parser');
 
-app.listen(port);
+const mc = mysql.createConnection({
+  host: host,
+  user: db_user,
+  password: db_pwd,
+  database: db
+})
+mc.connect();
+console.log('DB connected: ' + db);
 
+app.listen(port);
 console.log('Express server started on: ' + port);
 
 app.use(bodyParser.urlencoded({ extended: true }));
